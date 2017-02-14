d8d30c268db691fd039d1eab565ea12de18afb67
d-apurva <apurva.dhoble@click2cloud.net>
2017-02-14 16:04:21 +0530

    Update AppController.php

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -34,5 +34,5 @@ class AppController extends Controller {
 	/**
 	 * Add in the DebugKit toolbar
 	 */
-	public $components = rray('DebugKit.Toolbar');
+	public $components = array('DebugKit.Toolbar');
 }
