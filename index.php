b136720f094fba8f2f5b9c24976e9e2db658ac9a
work <work@DESKTOP-OV803F7.Home>
2017-09-09 18:08:17 -0300

    g

--- index.php
+++ index.php
@@ -1,41 +1,173 @@
-<?php
-/**
- * Requests collector.
- *
- *  This file collects requests if:
- *	- no mod_rewrite is available or .htaccess files are not supported
- *  - requires App.baseUrl to be uncommented in app/Config/core.php
- *	- app/webroot is not set as a document root.
- *
- * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://cakephp.org CakePHP(tm) Project
- * @since         CakePHP(tm) v 0.2.9
- * @license       http://www.opensource.org/licenses/mit-license.php MIT License
- */
+<!DOCTYPE HTML>
+<!--
+	Miniport by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>bpilon.tk</title>
+		<link rel="icon" type="image/png" href="images/icon.png">
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/jquery.scrolly.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/init.js"></script>
+		<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-/**
- *  Get CakePHP's root directory
- */
-define('APP_DIR', 'app');
-define('DS', DIRECTORY_SEPARATOR);
-define('ROOT', dirname(__FILE__));
-define('WEBROOT_DIR', 'webroot');
-define('WWW_ROOT', ROOT . DS . APP_DIR . DS . WEBROOT_DIR . DS);
+		ga('create', 'UA-61058643-1', 'auto');
+		ga('send', 'pageview');
+		</script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-desktop.css" />
+		</noscript>
+	</head>
+	<body>
+		<!-- Nav -->
+			<nav id="nav">
+				<nav class="container">
+					<ul style="text-align: left;">
+						<li><a href="index.php" class="icon fa-home"> Página Inicial<span class="label">Página Inicial</span></a></li>
+						<li><a href="#top"><i class="icon fa-arrow-circle-up"></i> Topo</a></li>
+						<li><a href="#work"><i class="icon fa-download"></i> Downloads</a></li>
+					</ul>
+					<ul style="text-align: right;">
+						<li><a href="#contact">Prof. Bruno Hernandes Azenha Pilon <i class="icon fa-angle-double-down"></i></a></li>
+					</ul>
+				</nav>
+			</nav> 
 
-/**
- * This only needs to be changed if the "cake" directory is located
- * outside of the distributed structure.
- * Full path to the directory containing "cake". Do not add trailing directory separator
- */
-if (!defined('CAKE_CORE_INCLUDE_PATH')) {
-	define('CAKE_CORE_INCLUDE_PATH', ROOT . DS . 'lib');
-}
+		<!-- Home -->
+			<div class="wrapper style5 first">
+				<article class="container" id="top">
+					<div class="row">
+						<div class="3u">
+						<span class="image fit"><img src="images/arduino.png" alt="" /></span>
+						</div>
+						<div class="9u">
+							<header>
+								<h1><strong>Eletrônica Digital I</strong></h1>
+								<p></p>
+							</header>
+							<a href="index.php" class="button big"><i class="icon fa-home"></i> Página Inicial</a>
+						</div>
+						
+					</div>
+				</article>
+			</div>
 
-require APP_DIR . DS . WEBROOT_DIR . DS . 'index.php';
+		<!-- Work -->
+			<div class="wrapper style2">
+				<article id="work">
+					<header>
+						<h2>Materiais para download</h2>
+						<p>2º Semestre de 2017</p>
+					</header>
+					<div class="container">
+						<div class="row">
+							<div class="6u">
+								<section class="box style1">
+									<span class="icon featured fa-book"></span>
+									<h3>Material Didático</h3>
+									<h4 style="text-align: left;"><a href="files/20171/Plano_de_Ensino.pdf" class="icon fa-file-pdf-o" target="_blank"> > Plano de Ensino</a></h4>	
+									<h4 style="text-align: left;"><a href="files/20171/Aula1CapacidadeMemoria.docx" class="icon fa-file-word-o" target="_blank"> > Aula 01 - Capacidade Memória</a></h4>
+									<h4 style="text-align: left;"><a href="files/20171/Aula2SistemasdeNumeracao.docx" class="icon fa-file-word-o" target="_blank"> > Aula 02 - Sistemas de Numeração</a></h4>
+									<h4 style="text-align: left;"><a href="files/20171/Aula3OutraFormadeConversaoentreBases.docx" class="icon fa-file-word-o" target="_blank"> > Aula 03 - Conversões entre Bases</a></h4>
+									<h4 style="text-align: left;"><a href="files/20171/Aula4AritmeticadosNumerosBinarios1.docx" class="icon fa-file-word-o" target="_blank"> > Aula 04 - Aritimética dos Binários 1</a></h4>
+									<h4 style="text-align: left;"><a href="files/20171/Aula41AritmeticadosNumerosBinarios.docx" class="icon fa-file-word-o" target="_blank"> > Aula 04.1 - Aritimética dos Binários 2</a></h4>
+									<h4 style="text-align: left;"><a href="files/20171/Aula5FuncoesLogicasPortasLogicas.docx" class="icon fa-file-word-o" target="_blank"> > Aula 05 - Funções Lógicas e Portas Lógicas 1</a></h4>
+									<h4 style="text-align: left;"><a href="files/20171/Aula51BlocoOuExclusivoeBlocoCoicidencia.docx" class="icon fa-file-word-o" target="_blank"> > Aula 05.1 - Funções Lógicas e Portas Lógicas 2</a></h4>
+									<h4 style="text-align: left;"><a href="files/20171/Aula6ObtendoExpressoesdeTabeTabdeExpressoesBooleanas.docx" class="icon fa-file-word-o" target="_blank"> > Aula 06 - Expressões Lógicas e Tabelas-Verdade</a></h4>
+									<h4 style="text-align: left;"><a href="files/20171/Aula9MapaKarnaugh.docx" class="icon fa-file-word-o" target="_blank"> > Aula 09 - Mapas de Karnaugh</a></h4>
+									<h4 style="text-align: left;"><a href="files/20171/Aula10CircuitosCombinacionais1Sinal.docx" class="icon fa-file-word-o" target="_blank"> > Aula 10 - Prática - Sinal de Trânsito</a></h4>
+									<h4 style="text-align: left;"><a href="files/20171/Aula10-ApProtoBoardSinalTransito.pptx" class="icon fa-file-powerpoint-o" target="_blank"> > Aula 10.1 - Protoboard - Sinal de Trânsito</a></h4>
+									<h4 style="text-align: left;"><a href="files/20171/Aula11CircCombP1AlarmeResidencial.docx" class="icon fa-file-word-o" target="_blank"> > Aula 11 - Alarme Residencial</a></h4>
+									<h4 style="text-align: left;"><a href="files/20171/Aula13CircCombP2BCD8421CI555.docx" class="icon fa-file-word-o" target="_blank"> > Aula 13 - Codificador BCD 8421 + Clock 555</a></h4>
+									<h4 style="text-align: left;"><a href="files/20171/Aula14CodBCD8421Disp7Seg.docx" class="icon fa-file-word-o" target="_blank"> > Aula 14 - Codificador BCD 8421 + Display 7 Segmentos</a></h4>
+									<h4 style="text-align: left;"><a href="files/20171/Aula15Codigos.docx" class="icon fa-file-word-o" target="_blank"> > Aula 15 - Codificações Binárias</a></h4>
+									<h4 style="text-align: left;"><a href="files/20171/Aula16CodBCD8421Johnson.docx" class="icon fa-file-word-o" target="_blank"> > Aula 16 - Codificador BCD 8421 p/ Johnson</a></h4>
+									<h4 style="text-align: left;"><a href="files/20171/Aula17SomadorCompleto.docx" class="icon fa-file-word-o" target="_blank"> > Aula 17 - Somador Completo</a></h4>
+									<h4 style="text-align: left;"><a href="files/20171/Aula18CircSeqFlipFlops.docx" class="icon fa-file-word-o" target="_blank"> > Aula 18 - Flip-Flops</a></h4>
+									<h4 style="text-align: left;"><a href="files/20171/Aula19ResgDesloc.docx" class="icon fa-file-word-o" target="_blank"> > Aula 19 - Registrador de Deslocamento</a></h4>
+									<h4 style="text-align: left;"><a href="files/20171/Aula20SinalTemporizado4017.docx" class="icon fa-file-word-o" target="_blank"> > Aula 20 - Sinal de Trânsito Temporizado</a></h4>
+									<h4 style="text-align: left;"><a href="files/20171/Aula21ContSeqQualquer.docx" class="icon fa-file-word-o" target="_blank"> > Aula 21 - Contador Síncrono Sequência Qualquer</a></h4>
+									<h4 style="text-align: left;"><a href="files/20171/Aula22ContAss.docx" class="icon fa-file-word-o" target="_blank"> > Aula 22 - Contadores Assíncronos</a></h4>
+									<h4 style="text-align: left;"><a href="files/20171/Aula23Contador0a594518.docx" class="icon fa-file-word-o" target="_blank"> > Aula 23 - Contador 00 à 59</a></h4>
+									<h4 style="text-align: left;"><a href="files/20171/Aula24ContCrescDecresc.docx" class="icon fa-file-word-o" target="_blank"> > Aula 24 - Contador Crescente-Decrescente</a></h4>
+									<h4 style="text-align: left;"><a href="files/20171/Aula25ConversoresDAeAD.docx" class="icon fa-file-word-o" target="_blank"> > Aula 25 - Conversor AD e DA</a></h4>
+									<h4 style="text-align: left;"><a href="files/20171/Aula26VoltDig.docx" class="icon fa-file-word-o" target="_blank"> > Aula 26 - Voltímetro Digital</a></h4>
+									<h4 style="text-align: left;"><a href="files/20171/Aula27MuxDemux.docx" class="icon fa-file-word-o" target="_blank"> > Aula 27 - Multiplex e Demultiplex</a></h4>
+									<h4 style="text-align: left;"><a href="files/20171/Aula28CombMultiplex.docx" class="icon fa-file-word-o" target="_blank"> > Aula 28 - Circ. Combinacionais c/ Multiplex</a></h4>
+									<h4 style="text-align: left;"><a href="files/20171/Aula29AmpCapMuxDemux.docx" class="icon fa-file-word-o" target="_blank"> > Aula 29 - Ampliação Mux-Demux</a></h4>
+								</section>
+							</div>
+							<!-- 
+							<div class="4u">
+								<section class="box style1">
+									<span class="icon featured fa-list-ol"></span>
+									<h3>Listas de Exercício</h3>
+									<h4 style="text-align: left;"><a href="files/20162/mic/Lista01.pdf" class="icon fa-file-pdf-o" target="_blank"> > Lista 01 - Memórias</a></h4>
+									<h4 style="text-align: left;"><a href="files/20162/mic/Lista02.pdf" class="icon fa-file-pdf-o" target="_blank"> > Lista 02 - Arduino</a></h4>
+									<hr></hr>
+									<h3>Projeto Final</h3>
+									<h4 style="text-align: left;"><a href="files/20162/mic/ProjetoFinalMicroPropostas.pdf" class="icon fa-file-pdf-o" target="_blank"> > Propostas</a></h4>
+								</section>
+							</div>
+							 -->
+							<div class="6u">
+								<section class="box style1">
+									<span class="icon featured fa-th"></span>
+									<h3>Diversos</h3>
+									<h4 style="text-align: left;"><a href="files/20171/EWB512.rar" class="icon fa-cloud-download" target="_blank"> > Simulador EWB 5.12 (Windows)</a></h4>
+									<h4 style="text-align: left;"><a href="files/20171/ApostilaETE.pdf" class="icon fa-file-pdf-o" target="_blank"> > Apostila Centro Paula Souza</a></h4>
+									<!-- <h4 style="text-align: left;"><a href="https://circuits.io/circuits/2948186-clock-com-555" class="icon fa-external-link-square" target="_blank"> > Simulador - Clock com 555</a></h4>
+									<h4 style="text-align: left;"><a href="files/20171/ExercA3.pdf" class="icon fa-file-pdf-o" target="_blank"> > Exercícios A3</a></h4>  -->
+								</section>
+							</div>
+						</div>
+					</div>
+				</article>
+			</div>
+
+		<!-- Contact -->
+			<div class="wrapper style4">
+				<article class="container" id="top">
+					<div class="row">
+						<div class="3u">
+						<span class="image"><img src="images/bhap.jpg" alt=""/></span>
+						</div>
+						<div class="9u">
+							<header>
+								<h1>Bruno Hernandes Azenha Pilon</h1>
+								<p>M.Sc. em Engenharia Elétrica <a href="http://lattes.cnpq.br/9763029283352977" class="icon fa-info-circle" target="_blank"><span class="label">LinkedIn</span></a></p>
+							</header>
+								<ul class="social">
+									<li><a href="https://www.facebook.com/pilon.bruno" class="icon fa-facebook-official" target="_blank"><span class="label">Facebook</span></a></li>
+									<li><a href="https://www.linkedin.com/pub/bruno-pilon/10/805/343" class="icon fa-linkedin" target="_blank"><span class="label">LinkedIn</span></a></li>
+								</ul>
+								<ul class="social">
+									<li><a href="https://plus.google.com/+BrunoPilon" class="icon fa-google-plus" target="_blank"><span class="label">Google+</span></a></li>
+									<li><a href="https://www.youtube.com/user/bhernandes" class="icon fa-youtube" target="_blank"><span class="label">YouTube</span></a></li>
+								</ul>
+						</div>
+					</div>
+				</article>
+				<article id="contact" class="container">
+					<div class="row">
+							<div class="12u">
+								<a href="http://www.etb.com.br" target="_blank"><img src="images/ETB.png" class="image" alt=""/></a>
+							</div>
+						</div>
+				</article>
+			</div>
+	</body>
+</html>
